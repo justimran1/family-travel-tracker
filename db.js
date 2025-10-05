039e9666a444d86131f7e2f557408b0ad3a30e72
OLUBODUN IMRAN <olubodunalade@gmail.com>
2025-10-06 00:12:55 +0100

    restored database on railway

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
-import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "World",
-  password: "olubodun112",
-  port: 5432,
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 db.connect();
 
